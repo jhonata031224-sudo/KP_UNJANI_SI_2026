eff36f76082de69ba9eb3a0abf93201bb4f02f86
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-09 22:01:20 +0700

    Terapkan sidebar submenu Danpus tanpa bullet

--- resources/views/siberad/dashboards/laporan-pimpinan-shell.blade.php
+++ resources/views/siberad/dashboards/laporan-pimpinan-shell.blade.php
@@ -1,2 +1,3 @@
 @include('siberad.dashboards.laporan-pimpinan')
+@include('siberad.dashboards.partials.danpus-sidebar-submenu-cleanup')
 @include('siberad.dashboards.partials.global-shell-enhancements')
