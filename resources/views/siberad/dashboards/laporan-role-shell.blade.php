875e3a8e5393df307300674e590eb573a918c063
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-11 08:01:43 +0700

    fix: hapus keterangan profil pada seluruh dashboard role

--- resources/views/siberad/dashboards/laporan-role-shell.blade.php
+++ resources/views/siberad/dashboards/laporan-role-shell.blade.php
@@ -1,3 +1,4 @@
 @include('siberad.dashboards.laporan-role')
 @include('siberad.dashboards.partials.global-shell-enhancements')
 @include('siberad.dashboards.partials.satlak-notification-close-text')
+@include('siberad.dashboards.partials.profile-description-hide')
