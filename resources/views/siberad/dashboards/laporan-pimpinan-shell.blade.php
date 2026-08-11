1ae7dd9650d95e58e0d12129eebe7ff11db033d4
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-11 07:00:29 +0700

    feat: aktifkan dropdown log aktivitas Danpus per perihal

--- resources/views/siberad/dashboards/laporan-pimpinan-shell.blade.php
+++ resources/views/siberad/dashboards/laporan-pimpinan-shell.blade.php
@@ -1,5 +1,6 @@
 @include('siberad.dashboards.laporan-pimpinan')
 @include('siberad.dashboards.partials.danpus-sidebar-submenu-cleanup')
 @include('siberad.dashboards.partials.danpus-ringkasan-submenu-hide')
+@include('siberad.dashboards.partials.danpus-activity-dropdown')
 @include('siberad.dashboards.partials.global-shell-enhancements')
 @include('siberad.dashboards.partials.danpus-monitoring-text-fix')
