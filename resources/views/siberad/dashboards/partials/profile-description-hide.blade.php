2f0ba7b1f008e014a02aeb7f07bfdba0e54a23a6
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-11 08:01:34 +0700

    fix: hapus keterangan pada profil seluruh role

---
+++ resources/views/siberad/dashboards/partials/profile-description-hide.blade.php
@@ -0,0 +1,7 @@
+<style>
+  /* Deskripsi profil tidak ditampilkan. Fungsi profil, avatar, pengaturan,
+     dan kontrol lainnya tetap dipertahankan. */
+  #profilePhotoView .profile-help-text {
+    display: none !important;
+  }
+</style>
