4f7dbafb32f942ae1c7f3c2c19458daa16573df4
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-18 09:39:40 +0700

    Update laporan-pimpinan-shell.blade.php

--- resources/views/siberad/dashboards/laporan-pimpinan-shell.blade.php
+++ resources/views/siberad/dashboards/laporan-pimpinan-shell.blade.php
@@ -1,4 +1,5 @@
 @include('siberad.dashboards.laporan-pimpinan')
+@include('siberad.dashboards.partials.log-aktivitas-realtime')
 @include('siberad.dashboards.partials.danpus-sidebar-submenu-cleanup')
 @include('siberad.dashboards.partials.danpus-ringkasan-submenu-hide')
 @include('siberad.dashboards.partials.danpus-activity-dropdown')
