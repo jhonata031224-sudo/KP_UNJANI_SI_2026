388c5fe9c3726b445922a33b9e87786b74035070
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-18 10:07:55 +0700

    Update LaporanController.php

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -8,7 +8,6 @@ use App\Models\PermintaanLaporan;
 use App\Models\Satuan;
 use App\Models\User;
 use App\Notifications\LaporanBaruDiterima;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
