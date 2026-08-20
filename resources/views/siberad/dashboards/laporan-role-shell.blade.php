eb5fb79d3868e4a39b41bff30b3445f8f6b41e15
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-20 09:38:09 +0700

    fix(role): stop Danpus table mutator from touching Satlak action buttons

--- resources/views/siberad/dashboards/laporan-role-shell.blade.php
+++ resources/views/siberad/dashboards/laporan-role-shell.blade.php
@@ -6,6 +6,5 @@
 @include('siberad.dashboards.partials.permintaan-laporan-deadline')
 @include('siberad.dashboards.partials.permintaan-laporan-realtime')
 @include('siberad.dashboards.partials.laporan-role-realtime-sync')
-@include('siberad.dashboards.partials.danpus-report-table-filter')
 @include('siberad.dashboards.partials.styled-select')
 @include('siberad.dashboards.partials.sidebar-header-surface')
