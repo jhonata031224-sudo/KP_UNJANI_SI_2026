709e2af3f3d815a85cd395871ad4c17c2b18ad24
jhonata031224-sudo <jhonata031224@gmail.com>
2026-08-21 14:24:25 +0700

    feat(admin): wire report center routes and safe UI middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\BackupFileController;
 use App\Http\Controllers\Admin\LandingAccessCaptchaController;
 use App\Http\Controllers\Admin\PermintaanResetPasswordController as AdminPermintaanResetPasswordController;
 use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\ReportCenterController;
 use App\Http\Controllers\Admin\ReportController;
 use App\Http\Controllers\Admin\SatuanController;
 use App\Http\Controllers\Admin\SessionController;
@@ -31,6 +32,7 @@ use App\Http\Controllers\PersonelMutasiController;
 use App\Http\Controllers\PostinganController;
 use App\Http\Controllers\ProfilFotoController;
 use App\Http\Controllers\ProyekRisetController;
+use App\Http\Middleware\InjectAdminReportCenter;
 use App\Http\Middleware\InjectDashboardUi;
 use App\Http\Middleware\InjectPengaturanAccessUi;
 use App\Http\Middleware\RemoveDecorativeSeparators;
@@ -59,6 +61,10 @@ return Application::configure(basePath: dirname(__DIR__))
                         ->name('pengaturan.access');
                     Route::get('/pengaturan/captcha', [LandingAccessCaptchaController::class, 'image'])
                         ->name('pengaturan.access-captcha');
+                    Route::get('/laporan/report-center/pengguna', [ReportCenterController::class, 'users'])
+                        ->name('laporan.report-center.users');
+                    Route::get('/laporan/report-center/aktivitas', [ReportCenterController::class, 'activities'])
+                        ->name('laporan.report-center.activities');
                 });
         },
     )
@@ -67,6 +73,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(RemoveDecorativeSeparators::class);
         $middleware->append(InjectDashboardUi::class);
         $middleware->append(InjectPengaturanAccessUi::class);
+        $middleware->append(InjectAdminReportCenter::class);
 
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
